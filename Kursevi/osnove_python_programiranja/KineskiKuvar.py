102d7dd63676e0e922ec0c2cc3cdc051b606ebc7
leemenix <leemenix@protonmail.com>
2020-04-13 06:52:42 -0700

    update python draft prirucnik

--- Kursevi/osnove_python_programiranja/KineskiKuvar.py
+++
@@ -1,8 +0,0 @@
-from Kuvar import Kuvar
-
-class KineskiKuvar(Kuvar):
-  def priprema_rize(self):
-    print("Priprema rize na kineski nacin")
-
-  def priprema_specijalnog_jela(self):
-  	print ("Pekinska patka")
--- Kursevi/osnove_python_programiranja/potapanje_broda.py
+++
@@ -1,8 +0,0 @@
-'''
-Viselinijski komentar
-'''
-"""
-Viselinijski komentar
-"""
-print("Komentari su korisni")
-# print("Ova linija koda nece biti ispisana")
